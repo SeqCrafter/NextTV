75096c54dc7cd0f2aeea141aab6a032af2b18682
ShineJ <34748231+ShineJiao@users.noreply.github.com>
2026-02-15 03:08:59 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,8 +17,8 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "NextTV - 影视无限",
-  description: "NextTV 影视播放平台",
+  title: "TV - 无限",
+  description: "TV平台",
 };
 
 export default function RootLayout({ children }) {
